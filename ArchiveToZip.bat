cac2e39533cc0c1801ded838a69e19d8cd7dfaff
QA350 <QA350@quantasylum.com>
2017-04-19 22:49:52 -0700

    Signed-off-by: QA350 <QA350@quantasylum.com>

--- ArchiveToZip.bat
+++
@@ -1,3 +0,0 @@
-"D:\Users\matttay\Documents\Visual Studio 2013\Projects\QAZipProjects\QAZipProjects\bin\Debug\QAZipProjects.exe" /X "D:\Users\matttay\Documents\Visual Studio 2013\Projects\QA350\Exclude.txt" /D "D:\Users\matttay\Documents\Visual Studio 2013\Projects\QA350"
-
-pause
